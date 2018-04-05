8b106d9026e196a31d96b8ba3c242dca42ba2658
Mielu89 <mielewczyk.mateusz@onet.pl>
2018-04-05 15:42:01 +0200

    Create tests for AddHoursView

--- worktime/tests/tests_views.py
+++ worktime/tests/tests_views.py
@@ -7,6 +7,15 @@ from job.models import *
 from worktime.models import *
 import datetime
 
+def createJobAndJobWorkersObjects(user):
+    
+    job = Job.objects.create(jobNr = 1, city = 'city', 
+                                     street = 'street%',
+                                     zip = '00-000',
+                                     start = datetime.date.today())
+    jobworker = JobWorker.objects.create(user = user, job = job)
+    return job, jobworker
+    
 class WorkTimeViewsSetUp:
         
     def setUp(self):
@@ -27,6 +36,7 @@ class MyJobsViewTest(WorkTimeViewsSetUp, TestCase):
     def test_correct_template(self):
         
         resp = self.client.get(reverse('worktime:myjobs'))
+        self.assertEquals(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'worktime/myjobs.html')
         
     def test_query_fo_search(self):
@@ -130,8 +140,69 @@ class MyJobsViewTest(WorkTimeViewsSetUp, TestCase):
 class AddHoursViewTest(WorkTimeViewsSetUp, TestCase):
             
     def test_redirect_user_not_login(self):
-        pass
-            
-            
-            
-            
+        
+        self.client.logout()
+          
+        resp = self.client.get(reverse('worktime:addhours'))
+        self.assertRedirects(resp, '/acc/login/?next=/hours/addhours/')
+        
+        resp = self.client.get(reverse('worktime:addhours', 
+                                       kwargs = {'jobNr': 1}))
+        self.assertRedirects(resp, '/acc/login/?next=/hours/addhours/1/')
+    
+    def test_correct_template(self):
+        
+        resp = self.client.get(reverse('worktime:addhours'))
+        self.assertEquals(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'worktime/addhours.html')
+    
+    def test_create_worktime_object_without_param_in_url(self):
+        
+        job, jobworkers = createJobAndJobWorkersObjects(self.user)
+        
+        resp =self.client.post(reverse('worktime:addhours'), 
+                                       {'description': 'description',
+                                       'date': datetime.date.today(),
+                                       'hours': 20,
+                                       'jobWorker': job.pk})
+        query = WorkTime.objects.all()
+        self.assertEquals(len(query), 1)
+        self.assertEquals(query[0].hours, 20)
+        
+    def test_create_worktime_object_with_param_in_url(self):
+        
+        createJobAndJobWorkersObjects(self.user)
+        
+        resp =self.client.post(reverse('worktime:addhours', 
+                                       kwargs ={'jobNr': 1}), 
+                                       {'description': 'description',
+                                       'date': datetime.date.today(),
+                                       'hours': 10})
+        query = WorkTime.objects.all()
+        self.assertEquals(len(query), 1)
+        self.assertEquals(query[0].hours, 10)
+    
+    def test_pass_kwargs_jobNr_to_class_form(self):
+        
+        createJobAndJobWorkersObjects(self.user)
+        
+        resp =self.client.post(reverse('worktime:addhours', 
+                                       kwargs ={'jobNr': 1}),
+                                       {'date': datetime.date.today()})
+
+        self.assertEquals(resp.context['form'].jobNr, 1)
+        
+    def test_redirect_after_save_object(self):
+        
+        createJobAndJobWorkersObjects(self.user)
+        
+        resp =self.client.post(reverse('worktime:addhours', 
+                                       kwargs ={'jobNr': 1}), 
+                                       {'description': 'description',
+                                       'date': datetime.date.today(),
+                                       'hours': 10})
+        self.assertRedirects(resp, reverse('worktime:myjobs'))
+        
+class MyHoursViewTest(WorkTimeViewsSetUp, TestCase):
+    
+    pass
